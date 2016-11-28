d65aa3a2c4da3a8e7ca81581d2e244d05c8b9dd5
DorciM <dora.medgyasszay@gmail.com>
2016-11-28 14:15:15 +0100

    connection with supplier table

--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -1,7 +1,109 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
 /**
  * Created by svindler on 28.11.2016.
  */
-public class SupplierDaoJdbc {
+public class SupplierDaoJdbc implements SupplierDao {
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "postgres";
+
+
+    @Override
+    public void add(Supplier supplier) {
+        String query = "INSERT INTO supplier (name) VALUES (?);";
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setString(1, supplier.getName());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+
+        String query = "SELECT * FROM supplier WHERE id ='" + id + "';";
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            if (resultSet.next()){
+                Supplier supplier = new Supplier(resultSet.getString("id"),
+                resultSet.getString("name"));
+
+                return supplier;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public void remove(int id) {
+
+        String query = "DELETE FROM suppliers WHERE id = '" + id +"';";
+        executeQuery(query);
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+
+        String query = "SELECT * FROM supplier;";
+
+        List<Supplier> supplierList = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Supplier supplier = new Supplier(resultSet.getString("id"),
+                        resultSet.getString("name"));
+                supplierList.add(supplier);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return supplierList;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
