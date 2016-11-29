1644fa0fa0b6a2422e9ede46f4f1ff7f1c60c271
DorciM <dora.medgyasszay@gmail.com>
2016-11-29 11:10:46 +0100

    product database connection

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -6,12 +6,13 @@ import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ProductDaoJdbc implements ProductDao {
 
-    private static final String DATABASE = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "postgres";
 
@@ -24,8 +25,9 @@ public class ProductDaoJdbc implements ProductDao {
             preparedStatement.setString(1, product.getName());
             preparedStatement.setFloat(2, product.getDefaultPrice());
             preparedStatement.setObject(3, product.getDefaultCurrency());
-            preparedStatement.setInt(4, product.getProductCategory().getId());
-            preparedStatement.setInt(5, product.getSupplier().getId());
+            preparedStatement.setString(4, product.getDescription());
+            preparedStatement.setInt(5, product.getProductCategory().getId());
+            preparedStatement.setInt(6, product.getSupplier().getId());
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -37,26 +39,119 @@ public class ProductDaoJdbc implements ProductDao {
     @Override
     public Product find(int id) {
 
+        String query = "SELECT * FROM product WHERE id ='" + id + "';";
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            if (resultSet.next()){
+                Product product = new Product(resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("category_id"),
+                        resultSet.getInt("supplier_id"));
+
+                return product;
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
     }
 
     @Override
     public void remove(int id) {
+
+        String query = "DELETE FROM product WHERE id = '" + id +"';";
+        executeQuery(query);
 
     }
 
     @Override
     public List<Product> getAll() {
 
+        String query = "SELECT * FROM product;";
+
+        List<Product> productList = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product product = new Product(resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("category_id"),
+                        resultSet.getInt("supplier_id"));
+                    productList.add(product);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return productList;
+
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
+
+        String query = "SELECT * FROM product WHERE id = '" + supplier.getId() +"';";
+
+        List<Product> productsBySupplier = new ArrayList<>();
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            while (resultSet.next()) {
+                Product product = new Product(resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("category_id"),
+                        resultSet.getInt("supplier_id"));
+                productsBySupplier.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productsBySupplier;
 
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
 
+        String query = "SELECT * FROM proruct WHERE id = '" + productCategory.getId() +"';";
+
+        List<Product> productsByCategory = new ArrayList<>();
+        try (Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            while (resultSet.next()) {
+                Product product = new Product(resultSet.getString("name"),
+                        resultSet.getFloat("default_price"),
+                        resultSet.getString("currency"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("category_id"),
+                        resultSet.getInt("supplier_id"));
+                productsByCategory.add(product);
+                }
+            }  catch (SQLException e) {
+            e.printStackTrace();
+        }
+            return productsByCategory;
     }
 
     private Connection getConnection() throws SQLException {
