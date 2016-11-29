e3280a4c4e3f9c61c89a423981523761239ca600
codecoolmz <vagasiferi4ever@gmail.com>
2016-11-29 11:10:59 +0100

    product category dao implementation with JDBC --not tested

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -1,7 +1,92 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.sql.*;
+import java.util.List;
+
 /**
  * Created by svindler on 28.11.2016.
  */
-public class ProductCategoryDaoJdbc {
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "postgres";
+
+    @Override
+    public void add(ProductCategory category){
+        String query = "INSERT INTO category (name, department, description) VALUES (?, ?, ?);";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+            preparedStatement.setString(1, category.getName());
+            preparedStatement.setString(2, category.getDepartment());
+            preparedStatement.setObject(3, category.getDescription());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    };
+
+    @Override
+    public ProductCategory find(int id){
+        String query = "SELECT * FROM category WHERE id ='" + id + "';";
+
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);)
+        {
+            if(resultSet.next()){
+                ProductCategory result = new ProductCategory(resultSet.getString("name"),
+                        resultSet.getString("department"),
+                        resultSet.getString("description"));
+                return result;
+            }else{
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    };
+
+    @Override
+    public void remove(int id){
+        String query = "DELETE FROM category WHERE id ='" + id + "';";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            
+            preparedStatement.executeQuery(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    };
+
+    @Override
+    public List<ProductCategory> getAll(){}
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    private void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
