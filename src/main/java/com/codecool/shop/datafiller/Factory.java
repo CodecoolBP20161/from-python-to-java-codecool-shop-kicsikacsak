360a7bc79ea375e09cbeb02e986f87b514201753
DorciM <dora.medgyasszay@gmail.com>
2016-12-01 11:04:05 +0100

    datastore type switcher

--- src/main/java/com/codecool/shop/datafiller/Factory.java
+++ src/main/java/com/codecool/shop/datafiller/Factory.java
@@ -1,9 +1,9 @@
 package com.codecool.shop.datafiller;
 
+import com.codecool.shop.controller.DataStoreSwitcher;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -17,6 +17,8 @@ public class Factory {
 
     private static Factory instance = null;
 
+    private static DataStoreSwitcher switcher = new DataStoreSwitcher();
+
     public static synchronized Factory getInstance() {
         if (instance == null) {
             instance = new Factory();
@@ -25,19 +27,20 @@ public class Factory {
     }
 
     public void newProduct(String name, float defaultPrice, String currencyString, String description, ProductCategory productCategory, Supplier supplier) {
-        ProductDao productDataStore = ProductDaoJdbc.getInstance();
+
+        ProductDao productDataStore = switcher.getProductDao();
         productDataStore.add(new Product(name, defaultPrice, currencyString, description, productCategory, supplier));
     }
 
     public ProductCategory productCategory(String name, String department, String description) {
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJdbc.getInstance();
+        ProductCategoryDao productCategoryDataStore = switcher.getProductCategoryDao();
         ProductCategory category = new ProductCategory(name, department, description);
         productCategoryDataStore.add(category);
         return category;
     }
 
     public Supplier newSupplier(String name, String description) {
-        SupplierDao supplierDataStore = SupplierDaoJdbc.getInstance();
+        SupplierDao supplierDataStore = switcher.getSupplierDao();
         Supplier supplier = new Supplier(name, description);
         supplierDataStore.add(supplier);
         return supplier;
